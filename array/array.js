c1becd71ce134d8a30cea64a3c656d414bf9b3c4
wannabewize <wannabewize@gmail.com>
2016-07-17 14:14:26 +0900

    몽고DB 예제들

--- array/array.js
+++ array/array.js
@@ -1,2 +1,9 @@
+use samples;
+
+db.array.remove({});
+
 db.array.insert({name:"IU"});
+// 배열에 추가
 db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
+db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
+
--- basic/insert.js
+++ basic/insert.js
@@ -1,2 +1,4 @@
-db.basic.insert({name1:"value1"})
-db.basic.insert({name2:"value2", score:1})
+use samples;
+
+db.basic.insert({name1:"value1"});
+db.basic.insert({name2:"value2", score:1});
