ef489adb9bf857513454ea3a637478393837db01
wannabewize <wannabewize@gmail.com>
2018-05-14 19:06:11 +0900

    array edit

--- model/array.js
+++ model/array.js
@@ -1,9 +1,19 @@
 use samples;
 
-db.array.remove({});
+// 새롭게 배열로 만들기
+db.singers.insert({name:"Imagine Dragons", songs: ["Believer", "Thunder", "Radioactive"]})
 
-db.array.insert({name:"IU"});
+db.singers.insert({name:"IU"})
 // 배열에 추가
-db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
-db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
+db.singers.update({name:"IU"}, {$push:{songs:"좋은날"}})
+db.singers.update({name:"IU"}, {$push:{songs:"금요일에 만나요"}})
+db.singers.update({name:"IU"}, {$push:{songs:"누구나 비밀은 있다"}})
 
+
+// 찾기
+db.singers.find({songs: "좋은날"})
+
+// 원소 제거
+db.singers.update({name:"IU"}, {$pull: {songs:"좋은날"}})
+// songs 에서 마지막 원소 제거
+db.singers.update({name:"IU"}, {$pop: {songs:1}})
