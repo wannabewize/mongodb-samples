f6c159c1e8ef9c42014c034782dd99cc0f604a5e
wannabewize <wannabewize@gmail.com>
2018-05-14 15:03:32 +0900

    ...

--- array/array.js
+++
@@ -1,9 +0,0 @@
-use samples;
-
-db.array.remove({});
-
-db.array.insert({name:"IU"});
-// 배열에 추가
-db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
-db.array.update({name:"IU"},{$push:{songs:"좋은날"}})
-
---
+++ basic/updateQuiz.js
@@ -0,0 +1,13 @@
+use samples
+
+db.idols.insertMany([
+   {name:'Mina', score:85},
+   {name:'Nayeon', score:77},
+   {name:'Jungyeon', score:90},
+   {name:'Momo', score:86},
+   {name:'Sana', score:96},
+   {name:'JIhyo', score:80},
+   {name:'Dahyun', score:82},
+   {name:'Chaeyoung', score:68},
+   {name:'Tzuyu', score:100}
+])
